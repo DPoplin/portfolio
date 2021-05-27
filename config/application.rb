712d8789facec501906bfd9e25ffeb8fcad013fe
Dani Poplin <danih37@gmail.com>
2021-05-27 12:58:28 -0500

    remove solidus

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ require 'active_storage/engine'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-Dotenv::Railtie.load
+# Dotenv::Railtie.load
 
 module Webshop
   class Application < Rails::Application
